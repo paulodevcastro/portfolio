d5e8e15f450996ca4e49f2ab168e970a987740dd
Lit <paulogui702@gmail.com>
2023-05-31 19:13:36 -0300

    css skills sub-card

---
+++ section/home/scripts.js
@@ -0,0 +1,9 @@
+// function to hide and show menu
+document.getElementById('btn-show-more');
+
+function showMenu(aniterminal){
+    var terminal = document.querySelector('.sub-card');
+    if(terminal.checkVisibility = 'hidden'){
+        terminal.style.visibility = 'visible';
+    }
+}
