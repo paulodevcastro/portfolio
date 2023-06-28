4e083e16faeeef28495544c38733e9c570b69d74
gee <paulogui702@gmail.com>
2023-06-27 21:35:30 -0300

    creating animation to small and high description - first

--- section/home/scripts.js
+++ section/home/scripts.js
@@ -3,3 +3,7 @@ var buttonShowCards = document.getElementById("btn-know-more");
 buttonShowCards.addEventListener("click", function(){
     document.querySelector(".cards-dev").classList.toggle("show-cards");
 })
+
+buttonShowCards.addEventListener("click", function(){
+    document.querySelector(".whoami").classList.toggle("showHighDescription");
+})
