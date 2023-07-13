c5a12272b48bbb692c852a156d17527e30c826dd
Gee <paulogui702@gmail.com>
2023-07-13 17:18:51 -0300

    put css on contact section

--- section/home/scripts.js
+++ section/home/scripts.js
@@ -3,12 +3,7 @@ var buttonShowCards = document.getElementById("btn-know-more");
 
 buttonShowCards.addEventListener("click", function(){
     document.querySelector(".cards-dev").classList.toggle("show-cards");
-})
-
-buttonShowCards.addEventListener("click", function(){
     document.querySelector(".whoami").classList.toggle("showHighDescription");
-})
-
-buttonShowCards.addEventListener("click", function(){
     document.querySelector(".projects-section").classList.toggle("showProjectSection");
+    document.querySelector(".contact-section").classList.toggle("showContactSection");
 })
